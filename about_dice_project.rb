a51e31db7693fddea9d33e597eea1df2b909403a
User <u@PhantomMachine.local>
2015-04-25 21:11:19 +0300

    about_dice_project.rb done

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,6 +6,16 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+  def roll param
+    @values = Array.new(param).map do
+     rand(6) + 1
+   end
+  end
+end
+
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
